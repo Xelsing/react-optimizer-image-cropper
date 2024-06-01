41a13bbff3a61806341314d8cb26229bca8e17fe
Ivan Yarigin <vano.03.06@gmail.com>
2024-06-01 03:35:31 +0300

    styles fix

--- src/ImageCropper.tsx
+++ src/ImageCropper.tsx
@@ -5,7 +5,7 @@ import 'react-image-crop/dist/ReactCrop.css';
 import { ReactCropProps } from 'react-image-crop/src/ReactCrop';
 import { canvasPreview } from './canvasPreview';
 import { useDebounceEffect } from './useDebounceEffect';
-import styles from './ImageCropper.module.scss';
+import './ImageCropper.css';
 
 export interface ImageCropperRef {
   // output blob format (default webp)
@@ -258,7 +258,7 @@ export const ImageCropper = forwardRef<ImageCropperRef, ImageCropperType>(
           ) : loader ? (
             loader // put your loader
           ) : (
-            <div className={styles.DefaultLoader}/>
+            <div className='DefaultLoader'/>
           )}
         </ReactCrop>
       </div>
